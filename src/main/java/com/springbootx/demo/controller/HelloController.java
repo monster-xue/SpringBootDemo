08efca58c4376fdb8c28ea132434165f8604e510
zhengwei3 <82572432@qq.com>
2021-03-25 11:31:40 +0800

    Initial commit

---
+++ src/main/java/com/springbootx/demo/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.springbootx.demo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    @CrossOrigin
+    public JSONObject hello(@RequestBody JSONObject param) {
+        System.out.println(JSONObject.toJSONString(param));
+        return param;
+    }
+
+    @RequestMapping("/ng-billing-ext/services/task/IotExportTaskAdd")
+    @ResponseBody
+    @CrossOrigin
+    public JSONObject test(@RequestBody JSONObject param) {
+        System.out.println(JSONObject.toJSONString(param));
+        return param;
+    }
+
+    public static void main(String[] args) {
+        String s = "1";
+        String s1 = "";
+        String key = String.format("%s-%s", s, s1);
+        System.out.println(key);
+    }
+
+
+}
