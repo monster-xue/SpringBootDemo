7cd8488e0d4198ca0d872fa81ef45fe52f924250
zhengwei3 <82572432@qq.com>
2021-03-25 14:23:10 +0800

    test

--- src/main/java/com/springbootx/demo/controller/TestController.java
+++ src/main/java/com/springbootx/demo/controller/TestController.java
@@ -1,7 +1,19 @@
 package com.springbootx.demo.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    @CrossOrigin
+    public JSONObject hello(@RequestBody JSONObject param) {
+        System.out.println(JSONObject.toJSONString(param));
+        return param;
+    }
 }
